90b39355de9b03b538e1fa224b155ec527d7b933
josephambrozich <josephambrozich@gmail.com>
2022-10-18 00:10:59 -0400

    Front end changes to ticket display
    Make the comments and ticket information look way better.

--- resources/views/ticket.blade.php
+++ resources/views/ticket.blade.php
@@ -9,17 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    {{$ticket->title}}<br>
-                    {{$ticket->content}}<br>
-                    {{$ticket->author}}<br>
-                    {{$ticket->tags}}<br>
+                <div style="float:right">By: <div style="">{{$ticket->author}}</div><br>
+                    Tags: <div style="color: #c9c6c6;">{{$ticket->tags}}</div></div><br>
+                <div style="font-size:25px">{{$ticket->title}}</div><br>
+                    <div style="font-size:20px">{{$ticket->content}}</div><br>
+                    
 
-                    Comments:
-                    </ul>
+                    
+                </div>
+
+                <div class="p-6 bg-white border-b border-gray-200">
+                Comments:
+                 <ul style="list-style: none;">
+
                     @foreach ($comments as $comment)
-                        <li>{{$comment->content}}, {{$comment->author}}</li>
+                        <li style="border: 1px solid gray;border-radius: 10px;font-size: inherit;padding: 10px;margin: 5px;">
+                            <div>{{$comment->content}}</div> <br> <div style="color: #c9c6c6;">{{$comment->author}}</div>
+                        </li>
                     @endforeach
                     </ul>
+
                 </div>
                 <form name="create-comment" id="create-comment" method="post" action="{{url('store-comment')}}">
                         @csrf
