e965e28b0c76de06b5aa671177b82c308d4bbb1d
josephambrozich <josephambrozich@gmail.com>
2022-11-27 15:13:38 -0500

    Redesigned dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,21 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in! <br>
+                     <br>
 
                     <form name="search-tickets" id="search-tickets" method="post" action="{{url('search-query')}}">
                         @csrf
                             <div class="form-group">
-                            <label for="exampleInputEmail1">Search</label><br>
-                            <input style="border: 1px solid #dfdbdb;width: 80%;margin: 5px;border-radius: 10px;padding: 10px;" type="text" id="search" name="search" class="form-control" required="">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <input style="border: 1px solid #dfdbdb;width: 80%;margin: 5px;margin-left:7%;border-radius: 10px;padding: 10px;" type="text" id="search" name="search" class="form-control" required="">
+                            <button style=" margin: 5px;padding: 7px; border-radius: 12px;font-size: 20px; background-color: #CF5C36;color: white;" type="submit" class="btn btn-primary">Search</button>    
+                        </div>
+                           
                         </form>
                     <br>
-                    <a href="{{ route('advSearch') }}">Advanced Search</a> <br>
-                    <br>
-                    <a href="{{ route('createPost') }}">Create A Post</a> <br>
-                    <a href="{{ route('tickets') }}">View All Posts</a>
+                    
+                    <a style="margin: 5px;margin-left:45%; padding: 7px;border-radius: 12px;font-size: 20px;background-color: inherit;color: #565656;filter: drop-shadow(5px 5px 5px lightgray);border: 1px solid #dfdbdb;" href="{{ route('tickets') }}">View All Posts</a>
                 </div>
             </div>
         </div>
